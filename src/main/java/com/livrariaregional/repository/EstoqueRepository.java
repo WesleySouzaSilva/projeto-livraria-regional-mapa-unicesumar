98b76f32fed948904db3de2523fc36bb422e18b8
Wesley Souza Silva <wesley@livrariaregional.local>
2026-06-25 17:29:33 -0300

    fix(pr3): ajusta repositories a update 4.1 (titulo -> nome)

--- src/main/java/com/livrariaregional/repository/EstoqueRepository.java
+++ src/main/java/com/livrariaregional/repository/EstoqueRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
 
-    List<Estoque> findByFilialOrderByProdutoTituloAsc(Filial filial);
+    List<Estoque> findByFilialOrderByProdutoNomeAsc(Filial filial);
 
     Optional<Estoque> findByFilialAndProduto(Filial filial, Produto produto);
 }
